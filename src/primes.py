858600bd8386c8b681875ac7a98f2f53b59499a2
Markus <marpe163@student.liu.se>
2021-05-08 16:11:21 +0200

    Add method for computing all primes up to a bound.

--- src/primes.py
+++ src/primes.py
@@ -7,3 +7,16 @@ def get_primes_trial_division(num):
         if num == 1:
             return primes
     return primes
+
+
+def is_prime(num):
+    for i in range((num // 2) + 1):
+        if i % num == 0:
+            return False
+    return True
+
+
+def get_primes_up_to(num):
+    return [x if is_prime(x) for x in range(num + 1)]
+
+
