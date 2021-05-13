8fb2f222bf2ce7830a86f55745d1d2925611a673
Markus <marpe163@student.liu.se>
2021-05-13 21:51:59 +0200

    Kill mutants identified by Mutation testing

--- tests/test_prime_sieve.py
+++ tests/test_prime_sieve.py
@@ -9,17 +9,20 @@ class TestPrimeSieve(unittest.TestCase):
     def setUp(self):
         self.sieve = sieve.PrimeSieve(1000)
 
-    def test_prime_sieve_is_prime_for_prime(self):
-        prime = 13
+    def test_prime_sieve_is_prime_for_primes(self):
+        primes = [2, 3, 5, 7, 11, 13, 991]
         expected = True
-        actual = self.sieve.is_prime(prime)
-        self.assertEqual(expected, actual)
+        for prime in primes:
+            actual = self.sieve.is_prime(prime)
+            self.assertEqual(expected, actual)
 
-    def test_prime_sieve_is_prime_for_non_prime(self):
-        non_prime = 14
+    def test_prime_sieve_is_prime_for_non_primes(self):
+        non_primes = [4, 6, 8 ,9 , 14]
         expected = False
-        actual = self.sieve.is_prime(non_prime)
-        self.assertEqual(expected, actual)
+        for non_prime in non_primes:
+            actual = self.sieve.is_prime(non_prime)
+            self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
