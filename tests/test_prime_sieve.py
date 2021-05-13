0f3dc98669bdcee8d3e625f09fffc117058b6cb3
Markus <marpe163@student.liu.se>
2021-05-13 13:59:10 +0200

    Add unit tests for prime sieve

---
+++ tests/test_prime_sieve.py
@@ -0,0 +1,25 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+
+import unittest
+import src.prime_sieve as sieve
+
+class TestPrimes(unittest.TestCase):
+    def setUp(self):
+        self.sieve = sieve.PrimeSieve(1000)
+
+    def test_prime_sieve_is_prime_for_prime(self):
+        prime = 13
+        expected = True
+        actual = self.sieve.is_prime(prime)
+        self.assertEqual(expected, actual)
+
+    def test_prime_sieve_is_prime_for_non_prime(self):
+        non_prime = 14
+        expected = False
+        actual = self.sieve.is_prime(non_prime)
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
